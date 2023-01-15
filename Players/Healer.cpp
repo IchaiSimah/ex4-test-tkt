5482530b1a6b534aab48d153b5129ee37910bf27
ichaiSimah <ichaisimah@gmail.com>
2023-01-15 02:47:15 +0200

    tkt

--- Players/Healer.cpp
+++ Players/Healer.cpp
@@ -7,6 +7,9 @@ Healer::Healer(const std::string &playerName): Player(playerName){}
 void Healer::addHp(int hpToAdd){
         if(hpToAdd > 0){
             m_HP += (DOUBLE_HEAL * hpToAdd);
+            if(m_HP > DEFAULT_MAX_HP){
+                m_HP = DEFAULT_MAX_HP;
+            }
         }
     }
     void Healer::printPlayerInfo(std::ostream& os) const{
